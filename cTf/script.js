8aba41d15867c2ff9a27b9059c0622c297f344dd
JIbo999 <81326773+JIbo999@users.noreply.github.com>
2023-03-24 23:32:54 +0400

    Update script.js

--- cTf/script.js
+++ cTf/script.js
@@ -8,10 +8,10 @@ celsius.oninput = () => {
     let output = (parseFloat(celsius.value) * 9) / 5 + 32;
     fahrenheit.value = parseFloat(output.toFixed(2));
 }; 
-//Celsius to Fahrenheit 
+//Celsius to Fahrenheit ^^
 
 fahrenheit.oninput = () => {
     let output = ((parseFloat(fahrenheit.value) -32) * 5) / 9;
     celsius.value = parseFloat(output.toFixed(2));
 };
-//Fahrenheit to Celsius
+//Fahrenheit to Celsius ^^
